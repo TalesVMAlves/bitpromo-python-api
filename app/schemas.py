357196d2d5370dd77a2d3cd4dad1c84e5b9c4dd8
Tales_Alves <tales.v.m.alves@gmail.com>
2025-12-04 19:54:16 -0300

    chore: cammel case, padrão usado no java

--- app/schemas.py
+++ app/schemas.py
@@ -3,16 +3,16 @@ from typing import Optional
 
 class ProdutoResponse(BaseModel):
     id: int
-    nome: str
+    nomeProduto: str
     categoria: str
     preco: float
     imagem: Optional[str] = None
     link: Optional[str] = None
-    score_similaridade: float
+    scoreSimilaridade: float
 
 class ProdutoVetorInput(BaseModel):
     id: int
-    nome_produto: str
+    nomeProduto: str
     categoria: str
     preco: float
     imagem: Optional[str] = None
